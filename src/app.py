db16af44acf3abe4c208ce916eec3c3cf2162a92
marfurta <anamariamarfurt@gmail.com>
2022-08-06 23:18:02 +0000

    ultimos antes de subir

--- src/app.py
+++ src/app.py
@@ -15,14 +15,9 @@ X=df.drop(columns=['Ticket','Survived']).copy()
 X['Sex']=X['Sex'].cat.codes
 X['Embarked']=X['Embarked'].cat.codes
 
-X_train,X_test,y_train,y_test=train_test_split(X,y,random_state=1107)
 
-best_clas=RandomForestClassifier(n_estimators= 1550,min_samples_split= 2,min_samples_leaf= 2,max_depth= 10,criterion= 'gini',
- bootstrap= True,class_weight=None)
 
- best_clas.fit(X_train,y_train)
- y_pred_best=best_clas.predict(X_test)
 
- import pickle
-filename = '/workspace/RandomForest/models/finalized_model.sav'
-pickle.dump(best_clas, open(filename, 'wb'))
+filename = '../models/finalized_model.sav'
+loaded_model = pickle.load(open(filename, 'rb'))
+
