8555fddfb6352630c8d3402dff8e977cf8bb7959
marfurta <anamariamarfurt@gmail.com>
2022-08-06 23:22:15 +0000

    final

--- src/app.py
+++ src/app.py
@@ -1,4 +1,3 @@
-
 url='https://raw.githubusercontent.com/4GeeksAcademy/random-forest-project-tutorial/main/titanic_train.csv'
 df = pd.read_csv(url, index_col=[0,3])
 
@@ -15,7 +14,14 @@ X=df.drop(columns=['Ticket','Survived']).copy()
 X['Sex']=X['Sex'].cat.codes
 X['Embarked']=X['Embarked'].cat.codes
 
+df[['Sex','Embarked','Survived']]=df[['Sex','Embarked','Survived']].astype('category')
 
+y=df['Survived']
+X=df.drop(columns=['Ticket','Survived']).copy()
+
+X['Sex']=X['Sex'].cat.codes
+
+X['Embarked']=X['Embarked'].cat.codes
 
 
 filename = '../models/finalized_model.sav'
